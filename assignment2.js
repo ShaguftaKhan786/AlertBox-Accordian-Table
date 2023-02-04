7f9fa8f3854c620da9cd77bbb461e7f11f158d17
ShaguftaKhan786 <112148853+ShaguftaKhan786@users.noreply.github.com>
2023-02-04 19:34:44 +0530

    initial

---
+++ assignment2.js
@@ -0,0 +1,38 @@
+
+
+
+function showbtn(){
+    document.getElementById("customerAlert").style.display='block';
+}
+
+function press(){
+    document.getElementById('customerAlert').style.display="none";
+}
+
+
+
+// .........closed..............
+
+ 
+function fun2(){
+    document.getElementById("demo2").style.display="block";
+}
+function fun3(){
+    document.getElementById("demo3").style.display="block";
+document.getElementById('demo2').style.display="none";
+}
+function fun4(){
+
+    document.getElementById("demo4").style.display="block";
+document.getElementById('demo3').style.display="none";
+}
+
+
+
+
+
+
+function fun6(){
+    document.getElementById('tbl').setAttribute('border','5');
+
+}
